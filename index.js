ad79490c19acc962cd48c3a8b6dd850fde860682
yankuan <yankuan@kongzhong.com>
2020-02-27 17:50:21 +0800

    add

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var http=require('http');
 
 var server=http.createServer(function(request,response){
-  response.write('<i>我是一个i标签aaa</i>');
-  response.write('吼吼吼');
-  response.end('hehehehe');
+    response.writeHeader(200,{
+        "Content-type":"text/html;charset=utf-8"
+    });
+    response.write('<i>我是一个i标签aaa</i>');
+    response.write('吼吼吼');
+    response.end('hehehehe');
 });
-  //listen(port,ip) --ip必须在本机真实存在
+console.log('test_demo start!')
 server.listen(4000,'127.0.0.1');
